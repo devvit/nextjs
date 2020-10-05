3eef6f405fea0ef021856419504b35a65515453f
win64 <admin@example.com>
2020-10-05 19:32:33 +0800

    server.js

---
+++ server.js
@@ -0,0 +1,69 @@
+//
+
+const next = require('next');
+const { parse } = require('url');
+const { createServer } = require('http');
+
+// const { Hub } = require('./src');
+
+const app = next({ dev: process.env['NODE_ENV'] !== 'production' });
+const handle = app.getRequestHandler();
+
+const getHttpPort = () => {
+  if (!!process.env['PORT']) {
+    return process.env['PORT'];
+  } else if (!!process.env['BLUEMIX_REGION'] || !!process.env['KUBERNETES_SERVICE_HOST']) {
+    return 8080;
+  }
+
+  return 4000;
+};
+
+app.prepare().then(() => {
+
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(getHttpPort(), (err) => {
+    if (err) throw err;
+    console.log('start ...');
+  });
+
+});
+
+/*
+const hub = new Hub({
+  service: `ws://0.0.0.0:${process.env['HTTP_PORT']}/ss`,
+  key: 'www.facebook.com',
+  presets: [
+    {
+      name: 'ss-base'
+    },
+    {
+      name: 'ss-stream-cipher',
+      params: {
+        method: 'none'
+      }
+    }
+  ],
+  log_level: 'error'
+});
+
+hub.run()
+  .then(() => {
+    const server = hub._tcpServer;
+
+    app.prepare().then(() => {
+      server.on('request', (req, res) => {
+        const parsedUrl = parse(req.url, true);
+        req._hub = hub;
+        handle(req, res, parsedUrl);
+      })
+    });
+
+// console.log(server);
+  })
+  .catch((err) => {
+    // console.error(err);
+  });
+  */
